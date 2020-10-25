47343202b251904f55b9f42d862ade8df2cd8dc3
busyhanklux <23571329h@gmail.com>
2020-10-26 02:16:47 +0800

    到clear(待續)

--- app/src/main/java/com/example/a201019/MainActivity.java
+++ app/src/main/java/com/example/a201019/MainActivity.java
@@ -1,17 +1,133 @@
 package com.example.a201019;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  MyDB db = null;
+
+    private final static String _ID   = "_id";
+    private final static String NAME  = "name";
+    private final static String PRICE = "price";
+
+    Button badd , bedit, bdel , bclear;
+    EditText edit_name , edit_price;
+    ListView LV;
+    Cursor cursor;
+    long myid; //儲存_id值
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_name = findViewById(R.id.edit_name);
+        edit_price= findViewById(R.id.edit_price);
+        LV =  findViewById(R.id.LV);
+        badd = findViewById(R.id.add);
+        bedit= findViewById(R.id.edit);
+        bdel = findViewById(R.id.delete);
+        bclear= findViewById(R.id.clear);
+
+        badd.setOnClickListener(listener);
+        bedit.setOnClickListener(listener);
+        bdel.setOnClickListener(listener);
+        bclear.setOnClickListener(listener);
+        LV.setOnClickListener(listener);
+
+        db = new MyDB(this);
+        db.open();
+        cursor = db.getAll();
+        UpdateAdapter(cursor);
+    }
+
+    private LV.OnItemClickListener LVlistener = new LV.OnItemClickListener()
+    {
+        public void onItemClick(AdapterView<?> parent, View v,int position,long ID)
+        {
+            ShowData(ID);
+            cursor.moveToFirst(position);
+        }
+    };
+
+    private void ShowData(long id)
+    {
+        Cursor c = db.get(id);
+        myid = id;
+        edit_name.setText(c.getString(1)); //name
+        edit_price.setText(""+c.getInt(2)); //price
+    }
+
+    protected void onDestory(){
+        super.onDestroy();
+        db.close();
+    }
+
+    private Button.OnClickListener listener = new Button.OnClickListener(){
+        public void onClick(View v){
+            try {
+                switch (v.getId()){
+                    case R.id.add: {    //新增
+                        int price = Integer.parseInt(edit_price.getText().toString());
+                        String name = edit_name.getText().toString();
+                        if (db.append(name, price) > 0) {
+                            cursor = db.getAll();
+                            UpdateAdapter(cursor);
+                            ClearEdit();
+                        }
+                        break; }
+                    case R.id.edit:{    //修改
+                        int price = Integer.parseInt(edit_price.getText().toString());
+                        String name = edit_name.getText().toString();
+                        if(db.update(myid,name,price))
+                        {
+                            cursor = db.getAll();
+                            UpdateAdapter(cursor);
+                        }
+                        break; }
+                    case R.id.delete:{
+                        if(cursor != null && cursor.getCount() >= 0){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setTitle("確定刪除");
+                            builder.setMessage("確定要刪除" + edit_name.getText() + "這筆資料?");
+                            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            });
+                            builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (db.delete(myid)){
+                                        cursor = db.getAll();
+                                        UpdateAdapter(cursor);
+                                        ClearEdit();
+                                    }
+                                }
+                            });
+                            builder.show();
+                        }
+                        break;}
+                    case R.id.clear:{
+
+                    }
+
+                }
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/a201019/MyDB.java
+++ app/src/main/java/com/example/a201019/MyDB.java
@@ -1,5 +1,9 @@
 package com.example.a201019;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 public class MyDB {
@@ -10,7 +14,63 @@ public class MyDB {
     private final static String NAME = "name";
     private final static String PRICE = "price";
 
+    //建立資料表欄位
     private final static String CREATE_TABLE =
             "CREATE TABLE " + TABLE_NAME +  "(" + _ID + "INTEGER PRIMARYKEY," +
                     NAME + "TEXT," + PRICE + "INTERGER)";
+
+    private Context mCtx = null;
+    public MyDB(Context ctx){
+        this.mCtx = ctx;
+    }
+
+    public void open() throws SQLException{
+        db = mCtx.openOrCreateDatabase(DATABASE_NAME,0,null);
+        try{
+            db.execSQL(CREATE_TABLE);
+        }catch (Exception e) {
+        }
+    }
+
+    public void close(){
+        db.close();
+    }
+
+  //  public Cursor getAll() {  //查詢所有資料，取出所有的欄位
+  //      return db.rawQuery("SELECT * FROM " + TABLE_NAME ,null);
+  //  }
+
+    public Cursor getAll() {  //查詢所有資料，只取出三個欄位
+        return db.query(TABLE_NAME,
+                new String[] {_ID,NAME,PRICE},
+                null,null,null,null,null,null);
+    }
+
+    public Cursor get(long rowId) throws SQLException{ //查指定ID，只取三個
+        Cursor mCursor = db.query(TABLE_NAME,
+                new String[] {_ID,NAME,PRICE},
+                _ID + "=" + rowId,null,null,null,null,null);
+        if(mCursor != null){
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    public long append(String name,int price){ //新增一筆資料
+        ContentValues args = new ContentValues();
+        args.put(NAME,name);
+        args.put(PRICE,price);
+        return db.insert(TABLE_NAME,null,args); //在空的格子填入?
+    }
+
+    public boolean delete(long rowID){
+        return db.delete(TABLE_NAME,_ID + "=" + rowID ,null) > 0;
+    }
+
+    public boolean update(long rowId, String name,int price){ //更新指定資料
+        ContentValues args = new ContentValues();
+        args.put(NAME,name);
+        args.put(PRICE,price);
+        return db.update(TABLE_NAME,args,_ID+ "=" + rowId , null) > 0;
+    }
 }
